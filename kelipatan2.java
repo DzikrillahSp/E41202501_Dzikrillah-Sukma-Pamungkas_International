db9a621e5041035ac421d7fc646abc9fc8f7943e
DzikrillahSp <dzikrillahsp@gmail.com>
2021-03-31 00:06:19 +0700

    kelanjutan week 4
    Dzikrillah Sukma Pamungkas
    E41202501
    TIF INTER

---
+++ kelipatan2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasperulanganfor;
+
+/**
+ *
+ * @author acer
+ */
+public class kelipatan2 {
+     public static void main (String [] args){
+      
+       // membuat variabel
+        int i = 0;
+        // membuat design awal
+        System.out.println("         Do_While          ");
+        System.out.println(" Bilangan kelipatan 2 (1-100)  ");
+        System.out.println(" ============================  ");
+        
+        // membuat kelipatan 2 
+        do{
+            System.out.print("  " + i);
+        i += 2;
+        
+        // membuat batas dari keliapatan
+        }
+        while (i <=100);
+         System.out.println("");
+    }
+    
+}
+

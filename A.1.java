3a93ecd333d8b6c0e28290afc18525ca438c8cc5
DzikrillahSp <dzikrillahsp@gmail.com>
2021-05-03 22:51:01 +0700

    Praktikum Week 7
    Dzikrillah Sukma Pamungkas
    E41202501
    TIF INTERNATIONAL

---
+++ A.1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dasarpewarisan;
+
+public class A {
+    int x;
+    int y;
+    void TampilkanNilaiXY (){
+        System.out.println("Nilai x :" + x + ", y : " + y);
+    }
+}
+
---
+++ demo.2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konstruktor;
+
+/**
+ *
+ * @author acer
+ */
+public class demo {
+    public static void main(String[] args){
+        C subOb = new C();
+      
+    }
+}
